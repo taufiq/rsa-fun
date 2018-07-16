cad9aa3a40e506deaa9a831792749ce816a88385
Taufiq Mohammed <taufiqmmhd@gmail.com>
2018-07-16 22:34:41 +0800

    Add reading in of keys

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
+from Crypto.PublicKey import RSA
+
+pub_key_filenames = list(filter(lambda x: x.endswith(".pem"), os.listdir("./challenge")))
+pub_keys = []
+
+os.chdir('challenge')
+
+for name in pub_key_filenames:
+    key_text = open(name, 'r').read()
+    pub_key = RSA.importKey(key_text)
+    print(f'{pub_key_filenames.index(name) }: ' , pub_key.n, f'e: {pub_key.e}')
+    pub_keys.append(pub_key)
+
 #GCD where a is the larger number
 def gcd(a, b):
     if b == 0:
